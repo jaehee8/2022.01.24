f2bbf5f9e1ad026dd19ef15d207114cf9d68eaed
jaehee8 <97264678+jaehee8@users.noreply.github.com>
2022-01-24 15:20:58 +0900

    배열 메서드

---
+++ test_3.js
@@ -0,0 +1,9 @@
+const basicArray = ["1", "2", "3", "4", "5", "6"];
+// ? : push()
+// * push는 배열 맨 뒤에 원소를 추가할 때 사용하는 메서드
+basicArray.push("박재희");
+console.log(basicArray);
+// ? pop()
+// * pop은 배열의 맨 뒷 원소값을 '삭제'할 때 사용하는 메서드
+basicArray.pop("박재희");
+console.log(basicArray);
